c28d4f26904813cc8ceaaea900c94fba45d2a930
Javier Cruz <javier.cruz@caltec.mx>
2016-05-04 16:25:08 -0500

    nuevo codigo

---
+++ app/src/main/java/mx/caltec/jsamples/activities/RetrofitActivity.java
@@ -0,0 +1,15 @@
+package mx.caltec.jsamples.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import mx.caltec.jsamples.R;
+
+public class RetrofitActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_retrofit);
+    }
+}
